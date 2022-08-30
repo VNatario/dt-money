9483c1dd627674ef145b5dd16dfada85ce2ba301
Vitor Natario <natariovitor@gmail.com>
2022-08-30 10:24:27 -0300

    feat: create styles/global.ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { GlobalStyle } from "./styles/global";
+
 export function App() {
   return (
     <div className="App">
       <h1>Hello Wolrld</h1>
+      <GlobalStyle />
     </div>
   );
 }
