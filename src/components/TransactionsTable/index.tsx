7997975b40b85bd3cc7347a7f8b347a7acee8f6f
Vitor Natario <natariovitor@gmail.com>
2022-08-31 10:56:37 -0300

    feat: crate transactionstable

---
+++ src/components/TransactionsTable/index.tsx
@@ -0,0 +1,33 @@
+import { Container } from "./styles";
+
+export function TransactionsTable() {
+  return (
+    <Container>
+      <table>
+        <thead>
+          <tr>
+            <th>Título</th>
+            <th>Valor</th>
+            <th>Categoria</th>
+            <th>Data</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr>
+            <td>Desenvolvimento de website</td>
+            <td className="deposit">R$ 12.000,00</td>
+            <td>Desenvolvimento</td>
+            <td>30/08/2022</td>
+          </tr>
+          <tr>
+            <td>Aluguel</td>
+            <td className="withdraw"> - R$ 1.200,00</td>
+            <td>Casa</td>
+            <td>30/08/2022</td>
+          </tr>
+        </tbody>
+      </table>
+    </Container>
+  );
+}
