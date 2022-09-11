acbd6985260e0accc9a7fba5ee15eaf29d199afa
Vitor Natario <natariovitor@gmail.com>
2022-09-11 20:59:15 -0300

    feat: creating hook useTransactions

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,5 +1,4 @@
-import { useContext } from "react";
-import { TransactionsContext } from "../../TransactionsContext";
+import { useTransactions } from "../../hooks/useTransactions";
 
 import { Container } from "./styles";
 
@@ -8,7 +7,7 @@ import outcomeImg from "../../assets/outcome.svg";
 import totalImg from "../../assets/total.svg";
 
 export function Summary() {
-  const { transactions } = useContext(TransactionsContext);
+  const { transactions } = useTransactions();
 
   const summary = transactions.reduce(
     (acc, transaction) => {
@@ -50,7 +49,7 @@ export function Summary() {
           <img src={outcomeImg} alt="Saídas" />
         </header>
         <strong>
-          -
+          -{" "}
           {new Intl.NumberFormat("pt-BR", {
             style: "currency",
             currency: "BRL",
